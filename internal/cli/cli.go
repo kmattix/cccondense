78fe5bcd8adb86360d0a5b901b084a9a95ab310b
Kevin Mattix <kevinmattix@proton.me>
2025-05-23 23:26:34 -0600

    added file validation

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"errors"
+	"strings"
 )
 
 type Flags struct {
@@ -10,6 +11,29 @@ type Flags struct {
 	Write     bool
 }
 
+const fileSuffix = "-condensed"
+
 func Run(args []string, flags Flags) error {
+	return errors.New("not implemented")
+}
+
+func validateExtension(path string) bool {
+	extensions := [...]string{"srt"}
+	tokens := strings.Split(path, ".")
+	extension := tokens[len(tokens)-1]
+	for _, e := range extensions {
+		if extension == e {
+			return true
+		}
+	}
+	return false
+}
+
+func handleFile(input string, output string, write bool) error {
+	//if output is empty this writes to current directory with fileSuffix
+	return errors.New("not implemented")
+}
+
+func handleDirectory(input string, output string, write bool) error {
 	return errors.New("not implemented")
 }
--- internal/cli/cli_test.go
+++ internal/cli/cli_test.go
@@ -17,3 +17,61 @@ func TestRun(t *testing.T) {
 			-w, --write			Overwrite the original file with the condensed version.
 	*/
 }
+
+func TestValidateExtensionNone(t *testing.T) {
+	data := ""
+	actual := validateExtension(data)
+	expected := false
+	if actual != expected {
+		t.Errorf("validateExtension(%s) = %v; want %v", data, actual, expected)
+	}
+}
+
+func TestValidateExtensionTrue(t *testing.T) {
+	data := "/path/to/my/.secret/cc.srt"
+	actual := validateExtension(data)
+	expected := true
+	if actual != expected {
+		t.Errorf("validateExtension(%s) = %v; want %v", data, actual, expected)
+	}
+}
+
+func TestValidateExtensionFalse(t *testing.T) {
+	data := "/path/to/my/.secret/audio.wav"
+	actual := validateExtension(data)
+	expected := false
+	if actual != expected {
+		t.Errorf("validateExtension(%s) = %v; want %v", data, actual, expected)
+	}
+}
+
+func TestHandleFile(t *testing.T) {
+
+}
+
+func TestHandleFileDest(t *testing.T) {
+
+}
+
+func TestHandleFileWrite(t *testing.T) {
+
+}
+func TestHandleFileWriteDest(t *testing.T) {
+
+}
+
+func TestHandleDirectory(t *testing.T) {
+
+}
+
+func TestHandleDirectoryWrite(t *testing.T) {
+
+}
+
+func TestHandleDirectoryDest(t *testing.T) {
+
+}
+
+func TestHandleDirectoryWriteDest(t *testing.T) {
+
+}
