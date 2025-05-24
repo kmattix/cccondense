aae3f3df392c987381a1b1051a02a6681bc22bd1
Kevin Mattix <kevinmattix@proton.me>
2025-05-23 20:18:09 -0600

    initial cli design

--- internal/cli/cli_test.go
+++ internal/cli/cli_test.go
@@ -3,5 +3,16 @@ package cli
 import "testing"
 
 func TestRun(t *testing.T) {
+	/*
+		Useage: cccondense [OPTION]... FILE... [DEST]
+				cccondense [OPTION]... DIRECTORY [DEST]
+			Condenses closed-caption file(s). When DEST is not specified it will save a new file to the
+			source DIRECTORY in the format of FILE-condensed.EXTENSTION
 
+		OPTION
+			-r, --recursive			condenses all closed-caption FILEs in a directory and their
+									contents recursively
+			-v, --verbose			displays the progress of what is being done
+			-w, --write				overwites on top of the source FILE that is being condensed in place
+	*/
 }
